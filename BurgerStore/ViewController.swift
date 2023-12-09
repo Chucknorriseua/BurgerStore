7da71892aa88a351e881adbcb622b22ab7a7ce31
Yevhenii <lostyahoo565@gmail.com>
2023-12-09 15:28:33 +0200

    Initial Commit

---
+++ BurgerStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerStore
+//
+//  Created by Евгений Полтавец on 09/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
